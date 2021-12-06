84805a63ace31e60292eb4abd84f322d95974f26
Wesley <wesleyadwebster@gmail.com>
2021-12-05 23:14:55 -0600

    Dynamic routing of events, nested events, and third nested events

---
+++ pages/events/[...slug].js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const FilteredEventsPage = (props) => {
+    return (
+        <div>
+            <h1>Filtered Events</h1>
+        </div>
+    );
+};
+
+
+export default FilteredEventsPage;
